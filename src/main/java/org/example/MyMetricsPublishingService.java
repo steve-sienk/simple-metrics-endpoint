639f4322a45ca0018fdbe3a1a8a667c18127ee2b
Michael Oleske <moleske@pivotal.io>
2019-08-22 15:35:37 -0700

    Simple server added
    Authored-by: Michael Oleske <moleske@pivotal.io>

--- src/main/java/org/example/MyMetricsPublishingService.java
+++ src/main/java/org/example/MyMetricsPublishingService.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
@@ -9,17 +16,38 @@ import org.apache.geode.metrics.MetricsSession;
 
 public class MyMetricsPublishingService implements MetricsPublishingService {
   private MetricsSession session;
-  private MeterRegistry registry;
+  private PrometheusMeterRegistry registry;
+  private HttpServer httpServer;
 
   @Override
   public void start(MetricsSession session) {
     this.session = session;
     registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
     this.session.addSubregistry(registry);
+
+    InetSocketAddress address = new InetSocketAddress(9000);
+    httpServer = null;
+    try {
+      httpServer = HttpServer.create(address, 0);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    HttpContext context = httpServer.createContext("/");
+    context.setHandler(this::requestHandler);
+    httpServer.start();
+  }
+
+  private void requestHandler(HttpExchange httpExchange) throws IOException {
+    final byte[] scrapeBytes = registry.scrape().getBytes();
+    httpExchange.sendResponseHeaders(200, scrapeBytes.length);
+    final OutputStream responseBody = httpExchange.getResponseBody();
+    responseBody.write(scrapeBytes);
+    responseBody.close();
   }
 
   @Override
   public void stop() {
     session.removeSubregistry(registry);
+    httpServer.stop(0);
   }
 }
--- src/test/java/org/example/MyMetricsPublishingServiceTest.java
+++ src/test/java/org/example/MyMetricsPublishingServiceTest.java
@@ -1,9 +1,21 @@
 package org.example;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
+import java.io.IOException;
+
 import io.micrometer.prometheus.PrometheusMeterRegistry;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.HttpHostConnectException;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -31,6 +43,33 @@ class MyMetricsPublishingServiceTest {
     subject.start(metricsSession);
 
     verify(metricsSession).addSubregistry(any(PrometheusMeterRegistry.class));
+
+    subject.stop();
+  }
+
+  @Test
+  void start_addsAnHttpEndpointThatReturnsStatusOK() throws IOException {
+    subject.start(metricsSession);
+
+    HttpGet request = new HttpGet("http://localhost:9000/");
+    HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+    assertThat(response.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_OK));
+
+    subject.stop();
+  }
+
+  @Test
+  void start_addsAnHttpEndpointThatContainsRegistryData() throws IOException {
+    subject.start(metricsSession);
+
+    HttpGet request = new HttpGet("http://localhost:9000/");
+    HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+    String responseBody = EntityUtils.toString(response.getEntity());
+    assertThat(responseBody, equalTo(""));
+
+    subject.stop();
   }
 
   @Test
@@ -39,5 +78,17 @@ class MyMetricsPublishingServiceTest {
     subject.stop();
 
     verify(metricsSession).removeSubregistry(any(PrometheusMeterRegistry.class));
+  }
+
+  @Test
+  void stop_hasNoHttpEndpointRunning() {
+    subject.start(metricsSession);
+    subject.stop();
+
+    HttpGet request = new HttpGet("http://localhost:9000/metrics");
+
+    assertThrows(HttpHostConnectException.class, () -> {
+      HttpClientBuilder.create().build().execute(request);
+    });
   }
 }
